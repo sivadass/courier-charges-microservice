25e747ad724f0cd469f191d05402ea144ee70d6b
Sivadass N <nsivadass@gmail.com>
2018-12-30 01:51:35 +0530

    Added Region data

---
+++ utils.js
@@ -0,0 +1,73 @@
+const regionData = [
+  {
+    "first-digit": 1,
+    "area-covered": [
+      "Delhi",
+      "Haryana",
+      "Punjab",
+      "Chandigarh",
+      "Himachal Pradesh",
+      "Jammu & Kashmir"
+    ],
+    pricing: 70
+  },
+  {
+    "first-digit": 2,
+    "area-covered": ["Uttar Pradesh", "Uttaranchal"],
+    pricing: 65
+  },
+  {
+    "first-digit": 3,
+    "area-covered": [
+      "Rajasthan",
+      "Gujarat",
+      "Daman & Diu",
+      "Dadra & Nagar Haveli"
+    ],
+    pricing: 60
+  },
+  {
+    "first-digit": 4,
+    "area-covered": ["Maharashtra", "Goa", "Madhya Pradesh", "Chhattisgarh"],
+    pricing: 50
+  },
+  {
+    "first-digit": 5,
+    "area-covered": ["Andhra Pradesh", "Karnataka"],
+    pricing: 40
+  },
+  {
+    "first-digit": 6,
+    "area-covered": [
+      "Tamilnadu",
+      "Kerala",
+      "Lakshadweep",
+      "Minicoy & Admindivi Islands",
+      "Pondicherry"
+    ],
+    pricing: 30
+  },
+  {
+    "first-digit": 7,
+    "area-covered": [
+      "Orissa",
+      "West Bengal",
+      "Assam",
+      "Arunachal Pradesh",
+      "Mizoram",
+      "Meghalaya",
+      "Nagaland",
+      "Tripura",
+      "Sikkim",
+      "Andaman & Nicobar Islands"
+    ],
+    pricing: 90
+  },
+  {
+    "first-digit": 8,
+    "area-covered": ["Bihar", "Jharkand"],
+    pricing: 80
+  }
+];
+
+module.exports = regionData;
